7c4d02c4a87dc0e5e467504d2f5c15c21ba1afe7
yakushev <ugr@bk.ru>
2022-07-05 12:16:50 +0500

    Success download page but sttp 3.6.2 don't support zio 2.0 only 2.0.0-RC6 https://github.com/softwaremill/sttp/pull/1469

--- adviser/src/main/scala/app/MainApp.scala
+++ adviser/src/main/scala/app/MainApp.scala
@@ -1,10 +1,27 @@
 package app
 
-
 import services.{FbDownloader, FbDownloaderImpl}
+import sttp.client3.HttpClientSyncBackend
+import sttp.client3.asynchttpclient.zio.{AsyncHttpClientZioBackend, SttpClient}
+import sttp.client3.httpclient.zio.HttpClientZioBackend
 import zio.Clock.ClockLive
 import zio.Console.ConsoleLive
+import zio.logging._
 import zio.{Clock, Console, Scope, ULayer, ZIO, ZIOAppArgs, ZIOAppDefault}
+
+/**
+ *
+ * The 3 Laws of ZIO Environment:
+  1. Methods inside service definitions (traits) should NEVER use the environment
+  2. Service implementations (classes) should accept all dependencies in constructor
+  3. All other code ('business logic') should use the environment to consume services https://t.co/iSWzMhotOv
+ *
+ */
+
+ /**
+  * GET and parse JSON using the ZIO async-http-client backend and circe
+  * https://sttp.softwaremill.com/en/latest/examples.html
+ */
 
 /**
  * Independent services for:
@@ -13,17 +30,29 @@ import zio.{Clock, Console, Scope, ULayer, ZIO, ZIOAppArgs, ZIOAppDefault}
  * 3) save Seq[Event] into database - not implemented yet
  * 4) State of last parsing ZIO.Ref[timestamp + Seq[Event]]
  * 5) bot core implementation (not reference to any services)
-
 */
 object MainApp extends ZIOAppDefault {
 
-  val parserEffect :ZIO[FbDownloader, Throwable, Unit] =
+  val parserEffect :ZIO[SttpClient with FbDownloader, Throwable, Unit] =
     for {
-     fbdown <- ZIO.service[FbDownloader]
-    _ <- fbdown.download("http://xz.ru")
+      console <- ZIO.console
+      fbdown <- ZIO.service[FbDownloader]
+      result <- fbdown.getUrlContent(url =
+         "https://line05w.bk6bba-resources.com/line/desktop/topEvents3?place=live&sysId=1&lang=ru&salt=33kcb6w4ydl56iud3p&supertop=4&scopeMarket=1600"
+      )
+      _ <- console.printLine(result)
   } yield ()
 
-  val mainApp: ZIO[Any, Throwable, Unit] = parserEffect.provide(FbDownloaderImpl.layer)
+  val mainApp: ZIO[Any, Throwable, Unit] = parserEffect.provide(
+    AsyncHttpClientZioBackend.layer(),
+    FbDownloaderImpl.layer)
+
+
+  /**
+   * https://zio.github.io/zio-logging/docs/overview/overview_index.html#slf4j-bridge
+   * import zio.logging.slf4j.Slf4jBridge
+   * program.provideCustom(Slf4jBridge.initialize)
+  */
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
     mainApp.exitCode
--- adviser/src/main/scala/services/FbDownloader.scala
+++ adviser/src/main/scala/services/FbDownloader.scala
@@ -1,7 +1,24 @@
 package services
 
-import zio.{Clock, Console, Task, TaskLayer, ULayer, URIO, URLayer, ZIO, ZLayer}
-
+import sttp.capabilities.WebSockets
+import sttp.capabilities.zio.ZioStreams
+import sttp.client3.{SttpBackend, UriContext, basicRequest}
+import sttp.client3.httpclient.zio.HttpClientZioBackend
+import zio.{Clock, Console, Scope, Task, TaskLayer, ULayer, URIO, URLayer, ZIO, ZLayer}
+import sttp.client3._
+import sttp.client3.circe._
+import sttp.client3.asynchttpclient.zio._
+import io.circe.generic.auto._
+import zio._
+import zio.Console
+/**
+ *
+ * The 3 Laws of ZIO Environment:
+  1. Methods inside service definitions (traits) should NEVER use the environment
+  2. Service implementations (classes) should accept all dependencies in constructor
+  3. All other code ('business logic') should use the environment to consume services https://t.co/iSWzMhotOv
+ *
+ */
 /**
  * Service pattern 2:
  * 1. define interface
@@ -11,33 +28,43 @@ import zio.{Clock, Console, Task, TaskLayer, ULayer, URIO, URLayer, ZIO, ZLayer}
  */
   //1. service interface - read Json string from given url.
   trait FbDownloader {
-    def download(url: String): Task[String]
+    def getUrlContent(url: String): Task[String]
   }
 
   //2. accessor method inside companion objects
   object FbDownloader {
     def download(url: String): ZIO[FbDownloader, Throwable, String] =
-      ZIO.serviceWithZIO(_.download(url))
+      ZIO.serviceWithZIO(_.getUrlContent(url))
   }
 
-  //3. implementation of service interface
-  case class FbDownloaderImpl(console: Console, clock: Clock) extends FbDownloader {
-    override def download(url: String): Task[String] =
+  //3. Service implementations (classes) should accept all dependencies in constructor
+  case class FbDownloaderImpl(console: Console, clock: Clock, client: SttpClient) extends FbDownloader {
+    override def getUrlContent(url: String): Task[String] =
       for {
         time <- clock.currentDateTime
         _ <- console.printLine(s"$time - $url")
-        srcJson <- ZIO.succeed("this isi a download json text.")
-      } yield srcJson
+        _ <- console.printLine("Begin request")
+        basicReq = basicRequest.post(uri"$url")
+        response <- client.send(basicReq)
+        _ <- console.printLine(s" response statusText    = ${response.statusText}")
+        _ <- console.printLine(s" response code          = ${response.code}")
+        respText = response.body.toString
+        _ <- console.printLine(s" response.body.toString = ${respText}")
+        _ <- console.printLine(s" response.body.isLeft   =  ${response.body.isLeft}")
+        _ <- console.printLine(s" response.body.isRight  =  ${response.body.isRight}")
+        res <- ZIO.succeed(response.body.toString)
+      } yield res
   }
 
   //4. converting service implementation into ZLayer
   object FbDownloaderImpl {
-    val layer: TaskLayer[FbDownloader] =
+    val layer: ZLayer[SttpClient,Throwable,FbDownloader] =
       ZLayer {
         for {
           console <- ZIO.console
           clock <- ZIO.clock
-        } yield FbDownloaderImpl(console,clock)
+          client <- ZIO.service[SttpClient]
+        } yield FbDownloaderImpl(console,clock,client)
       }
   }
 
